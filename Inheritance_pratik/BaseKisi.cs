d313367876e2501a41c53fca1d5d4a471ec7f382
kubra <kubra@DESKTOP-K1F6O3C>
2024-08-14 02:01:37 +0300

    Proje dosyası ekle.

---
+++ Inheritance_pratik/BaseKisi.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_pratik
+{
+    public class BaseKisi
+    {
+        // Base sınıf özellikleri
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+
+        // Base sınıf metot
+        public void AdSoyadYazdir()
+        {
+            Console.WriteLine($"Ad: {Ad}, Soyad: {Soyad}");
+        }
+    }
+
+    public class 
+        Ogrenci : BaseKisi
+    {
+        //öğrenci sınıfı için ek özellik
+        public int 
+            OgrenciNumarasi { get; set; }
+        //öğrenci sınıfı için ek metot
+        public void 
+            OgrenciBilgileriniYazdir()
+        {
+            Console.WriteLine($"Öğrenci Numarası: {OgrenciNumarasi}");
+            AdSoyadYazdir();  
+        }
+    }
+
+    public class Ogretmen : BaseKisi
+    {
+        //öğremen sınıfı için ek özellik
+        public decimal 
+            Maas { get; set; }
+
+        //öğretmen sınıfı için ek metot
+
+        public void 
+            OgretmenBilgileriniYazdir()
+        {
+            Console.WriteLine($"Maaş: {Maas} TL");
+            AdSoyadYazdir();  
+        }
+    }
+}
---
+++ Inheritance_pratik/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using Inheritance_pratik;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Öğrenci nesnesi oluşturma, değer atama
+
+        Ogrenci ogrenci1 = new Ogrenci();
+        ogrenci1.Ad = "xx";
+        ogrenci1.Soyad = "yy";
+        ogrenci1.OgrenciNumarasi = 123;
+
+        // Öğrenci bilgilerini yazdırma
+
+        Console.WriteLine("Öğrenci Bilgileri:");
+        ogrenci1.OgrenciBilgileriniYazdir();
+        Console.WriteLine(); 
+
+        //Öğretmen nesnesi oluşturma, değer atama
+
+        Ogretmen ogretmen1 = new Ogretmen();
+        ogretmen1.Ad = "aa";
+        ogretmen1.Soyad = "bb";
+        ogretmen1.Maas = 77;
+
+        //çğretmen bilgilerini yazdırma
+
+        Console.WriteLine("Öğretmen Bilgileri:");
+        ogretmen1.OgretmenBilgileriniYazdir();
+    }
+}
